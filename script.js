a6e7eadc9ee3925c4b0f53b7ac377d18d8f976bc
CorvionLabs <corvionlabs@gmail.com>
2026-07-27 05:41:44 -0600

    Initial Corvion Labs website with Google Analytics

---
+++ script.js
@@ -0,0 +1,19 @@
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+
+    });
+}, {
+    threshold: 0.15
+});
+
+fadeElements.forEach(element => {
+    observer.observe(element);
+});
